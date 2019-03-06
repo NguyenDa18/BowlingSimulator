474faadd146b7a0a3dff68a0dbe5b44c73ca6969
Danh Nguyen <danhnguyen267@gmail.com>
2019-03-06 01:05:29 -0800

    Implemented bowling class and working on unit tests

--- bowling.js
+++ bowling.js
@@ -20,33 +20,102 @@ class Bowling {
         this.frameRoll = this.frameRoll++;
     }
 
-    incrementScore() {
+    incrementScore(pins) {
         if (this.currentFrame > this.maxFrames) return;
         this.frameScores[this.currentFrame - 1] += pins;
     }
 
     scoreStrike() {
         this.frames[this.currentFrame - 1] = 'X';
+        this.applyStrikeBonus(this.maxPins);
+        this.applySpareBonus(this.maxPins);
         this.incrementFrame();
     }
 
-    scoreSpare() {
-        this.frames[this.currentFrame - 1] = 'S';
+    scoreFirstRoll(pins) {
+        this.remainingPins = this.remainingPins - pins;
+        this.applySpareBonus(pins);
+        this.applyStrikeBonus(pins);
         this.incrementFrame();
+    }
+
+    scoreSpare(pins) {
+        this.frames[this.currentFrame - 1] = '/';
+        this.applyStrikeBonus(pins);
+        this.incrementFrame();
+    }
+
+    scoreOpenFrame(pins) {
+        this.frames[this.currentFrame - 1] = (this.maxPins - this.remainingPins) + pins;
+        this.applyStrikeBonus(pins);
+        this.incrementFrame();
+    }
+
+    applySpareBonus(pins) {
+        // pins on first roll after spare are counted twice (on frame of spare)
+        if (this.frames[this.currentFrame - 2] === '/') this.frameScores[this.currentFrame - 2] += pins;
+    }
+
+    applyStrikeBonus(pins) {
+        // two rolls after strike are counted twice (on frame of spare)
+        if (this.frames[this.currentFrame - 3] === 'X' &&
+            this.frames[this.currentFrame - 2] === 'X' &&
+            this.frameRoll === 1 &&
+            this.currentFrame <= this.maxFrames + 2
+        ) {
+            this.frameScores[this.currentFrame - 3] += pins;
+        }
+        if (this.frames[this.currentFrame - 2] === 'X' && this.currentFrame <= this.maxFrames + 1) this.frameScores[this.currentFrame - 2] += pins;
     }
 
     isGameOver() {
         if (this.currentFrame <= this.maxFrames) return false;
-        if (this.frames[this.maxFrames - 1] !== 'X' && this.frames[this.maxFrames - 1] !== 'S') return true
+        if (this.frames[this.maxFrames - 1] !== 'X' && this.frames[this.maxFrames - 1] !== '/') return true
+
+        // Spare in last frame gets no more than bonus roll
+        if (this.frames[this.maxFrames - 1] === '/' && this.frameRoll > 1) return true;
+
+        // Bonus roll after spare in last frame may get strike but then game ends without new roll
+        if (this.frames[this.maxFrames - 1] === '/' && this.frames[this.maxFrames] === 'X') return true;
+
+        if (this.frames[this.maxFrames - 1] === 'X') {
+            // if the first bonus roll is not a strike then finish the bonus frame
+            if (this.frames[this.maxFrames] !== 'X' && this.currentFrame > this.maxFrames + 1) return true;
+      
+            if (this.frames[this.maxFrames] === 'X') {
+              // if the second bonus roll is a strike, but was still used, the game is over
+              if (this.frames[this.maxFrames + 1] !== 'X' && this.frameRoll > 1) return true;
+              // if the second bonus roll is a strike the game is over
+              if (this.frames[this.maxFrames + 1] === 'X') return true;
+            }
+        }
 
         return false
     }
 
-    roll(numPins) {
-        if (numPins < 0) throw new Error('Negative roll is invalid!');
-        if (numPins > this.remainingPins) throw new Error(`Only ${this.remainingPins} remaining`);
+    roll(pins) {
+        if (pins < 0) throw new Error('Negative roll is invalid');
+        if (pins > this.remainingPins) throw new Error(`Only ${this.remainingPins} remaining`);
         if (this.isGameOver()) throw new Error('Cannot roll after game is over');
-        this.incrementScore(numPins);
+        this.incrementScore(pins);
+
+        if (this.frameRoll === 1) {
+            if (pins === this.maxPins) {
+              this.scoreStrike();
+              this.startFrame();
+            } 
+            else { this.scoreFirstRoll(pins); }
+        } 
+        else {
+            if (pins === this.remainingPins) { this.scoreSpare(pins); } 
+            else { this.scoreOpenFrame(pins); }
+            this.initializeFrame();
+        }
+    }
+
+    score() {
+        if (!this.isGameOver()) throw new Error('Score cannot be taken until game is finished');
+        return this.frameScores.reduce((total, num) => (total + num));
     }
 
 }
--- test/bowling_test.js
+++ test/bowling_test.js
@@ -1,13 +1,32 @@
 const chai = require('chai');
 const assert = chai.assert;
 const Bowling = require('../bowling');
+const expect = require('chai').expect;
+const should = require('chai').should();
 
 describe('Bowling', function() {
-  describe('Check game scored correctly', function() {
+  describe('Check game is scored correctly', function() {
     it('should be able to score game with all zeros', function() {
-        const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        const rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
         const bowling = new Bowling();
         rolls.forEach(roll => bowling.roll(roll));
+        expect(bowling.score()).to.equal(0);
+    });
+
+    it('should be able to score a game with no strikes or spares', () => {
+      const rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+      const bowling = new Bowling();
+      rolls.forEach((roll) => { bowling.roll(roll); });
+      expect(bowling.score()).to.equal(90);
     });
   });
+});
+
+describe('Logic check', function() {
+  it('Should not be able to count negative points', function() {
+    const bowling = new Bowling();
+    const err = new Error('Negative roll is invalid');
+    expect(() => { bowling.roll(-1); }).to.throw(err);
+
+  })
 });
